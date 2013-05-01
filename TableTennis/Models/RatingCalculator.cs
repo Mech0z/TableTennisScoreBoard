f35f544184b598359ead3c5c84daabadf7b6f027
Mads Skipper Pedersen <msp@d60.dk>
2013-05-01 22:45:55 +0200

    Added method to recalculate scores

--- TableTennis/Models/RatingCalculator.cs
+++ TableTennis/Models/RatingCalculator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using TableTennis.Interfaces.Repository;
 
@@ -12,7 +9,8 @@ namespace TableTennis.Models
         private readonly IMatchManagementRepository _matchManagementRepository;
         private readonly IPlayerManagementRepository _playerManagementRepository;
 
-        public RatingCalculator(IMatchManagementRepository matchManagementRepository, IPlayerManagementRepository playerManagementRepository)
+        public RatingCalculator(IMatchManagementRepository matchManagementRepository,
+                                IPlayerManagementRepository playerManagementRepository)
         {
             _matchManagementRepository = matchManagementRepository;
             _playerManagementRepository = playerManagementRepository;
@@ -22,6 +20,36 @@ namespace TableTennis.Models
         public void RecalculateRatings()
         {
             var allGames = _matchManagementRepository.GetAllGames();
+            var players = _playerManagementRepository.GetAllPlayers();
+
+            foreach (var player in players)
+            {
+                player.Rating = 1500;
+            }
+            
+            foreach (var game in allGames)
+            {
+                var player1 = players.SingleOrDefault(s => s.Id == game.PlayerIds[0]);
+                var player2 = players.SingleOrDefault(s => s.Id == game.PlayerIds[1]);
+
+                if (game.PlayerIds[0] == game.WinnerId)
+                {
+                    var elo = new EloRating(player1.Rating, player2.Rating, 1, 0);
+                    player1.Rating += (int) elo.Point1;
+                    player2.Rating += (int) elo.Point2;
+                }
+                else
+                {
+                    var elo = new EloRating(player1.Rating, player2.Rating, 0, 1);
+                    player1.Rating += (int)elo.Point1;
+                    player2.Rating += (int)elo.Point2;
+                }
+            }
+
+            foreach (var player in players)
+            {
+                _playerManagementRepository.UpdateRating(player.Id, player.Rating);
+            }
         }
     }
 }
