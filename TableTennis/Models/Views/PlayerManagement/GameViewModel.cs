cc1ca45406a470e89697a0f4b9d5c4aae05dd74a
Mads Skipper Pedersen <madsskipper@gmail.com>
2013-06-10 22:13:49 +0200

    reworked details site a bit

--- TableTennis/Models/Views/PlayerManagement/GameViewModel.cs
+++ TableTennis/Models/Views/PlayerManagement/GameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TableTennis.HelperClasses;
 
 namespace TableTennis.Models.Views.PlayerManagement
 {
--- TableTennis/ViewModels/PlayedGamesViewModel.cs
+++ TableTennis/ViewModels/PlayedGamesViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using TableTennis.Models;
-using TableTennis.Models.Views.PlayerManagement;
 
 namespace TableTennis.ViewModels
 {
@@ -9,30 +8,11 @@ namespace TableTennis.ViewModels
     {
         public PlayedGamesViewModel(List<PlayedGame> playedGames, string Username)
         {
-            GameList = new List<GameViewModel>();
-
-            foreach (var game in playedGames)
-            {
-                var opponentUsername = game.Players[0] == Username ? game.Players[1] : game.Players[0];
-                var won = game.WinnerUsername == Username;
-                var gameSets = game.GameSets ?? new List<GameSet>();
-
-                var gameVM = new GameViewModel
-                    {
-                        TimeStamp = game.TimeStamp,
-                        Opponent = opponentUsername,
-                        Won = won,
-                        Rating = game.EloPoints,
-                        GameSets = gameSets
-                    };
-
-                GameList.Add(gameVM);
-            }
-
-            GameList = GameList.OrderByDescending(s => s.TimeStamp).Take(10).ToList();
+            GameList = playedGames.OrderByDescending(s => s.TimeStamp).Take(10).ToList();
+            PlayerName = Username;
         }
 
-        public List<GameViewModel> GameList { get; set; }
-        public List<PlayerUsername> PlayerUsernames { get; set; }
+        public List<PlayedGame> GameList { get; set; }
+        public string PlayerName { get; set; }
     }
 }
