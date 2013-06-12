0547afc80af319068f34f90e7a8bdfbbf02ceb1e
Mads Skipper Pedersen <madsskipper@gmail.com>
2013-06-12 11:13:50 +0200

    Added recalculation of rating for double TT

--- TableTennis/HelperClasses/RatingCalculator.cs
+++ TableTennis/HelperClasses/RatingCalculator.cs
@@ -72,17 +72,61 @@ namespace TableTennis.HelperClasses
 
         public void RecalculateDoubleTTRatings()
         {
-            var allGames = _matchManagementRepository.GetAllGames();
+            var allGames = _matchManagementRepository.GetAllGames(Game.DoubleTableTennis).OrderBy(games => games.TimeStamp);
             var players = _playerManagementRepository.GetAllPlayers();
 
             foreach (var player in players)
             {
                 if (player.Ratings.ContainsKey(Game.DoubleTableTennis))
                 {
-                    player.Ratings[Game.SingleTableTennis] = 1500;
+                    player.Ratings[Game.DoubleTableTennis] = 1500;
+                }
+            }
+            foreach (var game in allGames)
+            {
+                if (!game.Ranked)
+                {
+                    continue;
+                }
+                var player1 = players.SingleOrDefault(s => s.Username == game.Players[0]);
+                var player2 = players.SingleOrDefault(s => s.Username == game.Players[1]);
+                var player3 = players.SingleOrDefault(s => s.Username == game.Players[2]);
+                var player4 = players.SingleOrDefault(s => s.Username == game.Players[3]);
+                int rating;
+
+                if (game.Players[0] == game.WinnerUsername)
+                {
+                    var elo = new EloRating(player1.Ratings[Game.DoubleTableTennis], player2.Ratings[Game.DoubleTableTennis], 1, 0);
+                    player1.Ratings[Game.DoubleTableTennis] += (int)elo.Point1;
+                    player2.Ratings[Game.DoubleTableTennis] += (int)elo.Point1;
+                    player3.Ratings[Game.DoubleTableTennis] += (int)elo.Point2;
+                    player4.Ratings[Game.DoubleTableTennis] += (int)elo.Point2;
+                    rating = (int)elo.Point1;
+                }
+                else
+                {
+                    var elo = new EloRating(player1.Ratings[Game.DoubleTableTennis], player2.Ratings[Game.DoubleTableTennis], 0, 1);
+                    player1.Ratings[Game.DoubleTableTennis] += (int)elo.Point1;
+                    player2.Ratings[Game.DoubleTableTennis] += (int)elo.Point1;
+                    player3.Ratings[Game.DoubleTableTennis] += (int)elo.Point2;
+                    player4.Ratings[Game.DoubleTableTennis] += (int)elo.Point2;
+                    rating = (int)elo.Point2;
+                }
+
+                game.EloPoints = rating;
+
+                _matchManagementRepository.UpdateGameRatingById(game);
+            }
+            foreach (var player in players)
+            {
+                if (player.Ratings.ContainsKey(Game.DoubleTableTennis))
+                {
+                    _playerManagementRepository.UpdateRating(player.Username, player.Ratings[Game.DoubleTableTennis],
+                                                             Game.DoubleTableTennis);
                 }
             }
         }
+
 
         public void RecalculateDoubleFoosballRatings()
         {
